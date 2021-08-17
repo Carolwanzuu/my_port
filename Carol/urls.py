67e6ccf4ea1eacd569e6cdfb5b65373984bb3672
Carolwanzuu <carolkyalo01@gmail.com>
2021-08-17 11:25:58 +0300

    edit url paths

--- Carol/urls.py
+++ Carol/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name='index')
 ]
--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Carol.urls'))
+    path('', include('Carol.urls')),
 ]
