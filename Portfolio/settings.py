86c027bee316e9fcd9112d5d2c40bb555a782d1d
Carolwanzuu <carolkyalo01@gmail.com>
2021-08-17 11:05:28 +0300

    editing about context

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -166,5 +166,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
